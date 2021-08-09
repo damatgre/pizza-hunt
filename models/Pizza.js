2735082859a47f88de5c0d3cb4328e83bbebf82f
damatgre <gregdamato2@gmail.com>
2021-08-09 10:07:45 -0400

    comments functionality and routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,28 +1,51 @@
 //import dependencies
 const { Schema, model } = require('mongoose');
 
+//import dateFormat function
+const dateFormat = require('../utils/dateFormat');
+
 //create schema using constructor from Mongoose and define fields wuth specific data type
-const PizzaSchema = new Schema({
-    
-    pizzaName: {
-        //instruct that data will adhere to built-in JS data types
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            //instruct that data will adhere to built-in JS data types
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            //if no value is provided when user creates data, this will be executed
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        //indicates array as data type, can also specify Array instead of brakcets
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        //if no value is provided when user creates data, this will be executed
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    //indicates array as data type, can also specify Array instead of brakcets
-    toppings: []
-});
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+//get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+    return this.comments.length;
+})
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
